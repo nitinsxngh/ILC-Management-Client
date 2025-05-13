c4e32e1bddbacdae77d8265faacf1f9c7b73e529
Nitin Singh <nitinsingh@Nitins-MacBook-Pro.local>
2025-05-13 16:28:50 +0530

    Endpoint Change

--- src/configs/config.js
+++ src/configs/config.js
@@ -1,8 +1,8 @@
 // config.js
 
 // API Endpoints
-// export const API_BASE_URL = "https://api.ilc.limited/api";
-export const API_BASE_URL = "http://localhost:5002/api";
+ export const API_BASE_URL = "https://api.ilc.limited/api";
+// export const API_BASE_URL = "http://localhost:5002/api";
 
 // Cloudinary Configuration
 export const CLOUDINARY_CLOUD_NAME = "dvztynz8i"; // Your Cloudinary cloud name
